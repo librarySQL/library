f4e38f08cc4a7716449270b9a0ce4b844f067406
eva1617 <105513556+eva1617@users.noreply.github.com>
2023-12-15 11:26:09 +0800

    Add files via upload

---
+++ delete_reservation.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+
+if (isset($_SESSION['account'])) {
+    $userId = $_SESSION['account'];
+} else {
+    header("Location: login.php");
+    exit;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['reservationId'])) {
+    $reservationId = $_GET['reservationId'];
+    
+    // 建立連線
+    $con = new mysqli("localhost", "root", "eva65348642", "librarydb");
+
+    if ($con->connect_error) {
+        die("Connection failed: " . $con->connect_error);
+    }
+
+    // 刪除預約
+    $deleteQuery = "DELETE FROM reservation WHERE Reservation_Id = '$reservationId'";
+    $result = $con->query($deleteQuery);
+
+    if ($result === TRUE && $con->affected_rows > 0) {
+        // 刪除成功，返回到原來的頁面
+        header("Location: reservation.php");
+        exit;
+    } else {
+        echo "刪除預約時發生錯誤：" . $con->error;
+    }
+
+    $con->close();
+} else {
+    echo "無效的請求";
+}
+?>
