31b914399023a7062e39ea3162a6fadb03e8db53
sharon1602 <116973213+sharon1602@users.noreply.github.com>
2023-12-17 18:59:16 +0800

    Update search_seat.php

--- search_seat.php
+++ search_seat.php
@@ -73,62 +73,52 @@ if (isset($_SESSION['account']) ) {
     <div class="container" style="width: 700px;margin: 0px auto; top:50px; margin-bottom 200px; font-family:Microsoft JhengHei;">
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     <br><br>
-<div align="center">
-    <label for="seatname">請選擇想查詢的座位編號:</label>
-    <br><br>
-    <select id="seatname" name="seatname" onchange="fetchReservation()">
-        <?php
+        <div align="center">
+            <label for="seatname">請選擇想查詢的座位編號:</label>
+            <br><br>
+            <select id="seatname" name="seatname" onchange="fetchReservation()">
+            <?php
+                // 获取所有座位名称
+                $seat_query = "SELECT Seat_Name FROM seat";
+                $seat_result = $con->query($seat_query);
 
-        // 檢查是否有選擇座位
-        if (isset($_POST['seatname']) && !empty($_POST['seatname'])) {
-            $selected_seat = $_POST['seatname'];
-
-            // 在選擇座位的 SQL 查詢中加入預約時間的條件
-            $filtered_seats_query = "SELECT * FROM seat";
-            $filtered_seats_result = $con->query($filtered_seats_query); // 修改此處的 $conn 為 $con
-
-            if ($filtered_seats_result->num_rows > 0) {
-                while ($row = $filtered_seats_result->fetch_assoc()) {
-                    $seat = $row['Seat_Name'];
-                    $selected = ($seat == $seat_name) ? 'selected' : '';
-                    echo "<option value='$seat' $selected>$seat</option>";
+                if ($seat_result->num_rows > 0) {
+                    while ($row = $seat_result->fetch_assoc()) {
+                        $seat = $row['Seat_Name'];
+                        echo "<option value='$seat'>$seat</option>";
+                    }
+                } else {
+                    echo "<option value=''>沒有符合條件的座位</option>";
                 }
-            } else {
-                echo "<option value=''>沒有符合條件的座位</option>";
-            }
-        }else {
-            // 如果未接收到 seatname 參數，可以採取相應的錯誤處理措施
-            echo "未接收到座位名稱參數";
-        }
-        
+                ?>
+            </select>
+        </div>
 
-        ?>
-    </select>
-</div>
+        <div style="text-align: center;">
+        <div id="reservation_info" style="display: inline-block; text-align: center; margin-top: 20px;"></div>
+        </div>
 
-<div style="text-align: center;">
-<div id="reservation_info" style="display: inline-block; text-align: center; margin-top: 20px;"></div>
-</div>
+    <br><br>
+        <div align="center">
+            <input type="submit" value="查詢可預約座位" style="width:30%;height: 40px;">
+        </div>
+    </div>
+</form>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
-function fetchReservation() {
-    var selectedSeat = $("#seatname").val();
+    <script>
+        function fetchReservation() {
+            var selectedSeat = $("#seatname").val();
 
-    $.ajax({
-        type: "POST",
-        url: "fetch_reservation.php",
-        data: { seatname: selectedSeat },
-        success: function(response) {
-            $('#reservation_info').html(response);
+            $.ajax({
+                type: "POST",
+                url: "fetch_reservation.php",
+                data: { seatname: selectedSeat },
+                success: function(response) {
+                    $('#reservation_info').html(response);
+                }
+            });
         }
-    });
-}
-</script>
-<br><br>
-<div align="center">
-    <input type="submit" value="查詢可預約座位" style="width:30%;height: 40px;">
-</div>
-</form>
+    </script>
 </body>
 </html>
