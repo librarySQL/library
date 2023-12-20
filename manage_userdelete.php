b5fb0b1bba7718edb9f4e9c8b98ad46e2203737e
yogachiu <153476414+yogachiu@users.noreply.github.com>
2023-12-21 00:13:23 +0800

    Create manage_userdelete.php

---
+++ manage_userdelete.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+
+if (isset($_SESSION['account'])) {
+    $userId = $_SESSION['account'];
+} else {
+    header("Location: login.php");
+    exit;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['userId'])) {
+    $userId = $_GET['userId'];
+
+    // 建立連線
+    $con = new mysqli("localhost", "root", "jenny104408!", "libdb");
+
+    if ($con->connect_error) {
+        die("Connection failed: " . $con->connect_error);
+    }
+
+    // 刪除預約
+    $deleteQuery = "DELETE FROM User WHERE User_Id = '$userId'";
+    $result = $con->query($deleteQuery);
+
+    if ($result === TRUE && $con->affected_rows > 0) {
+        // 刪除成功，返回到原來的頁面
+        header("Location: ../manager/manage_user.php");
+        exit;
+    } else {
+        echo "刪除預約時發生錯誤：" . $con->error;
+    }
+
+    $con->close();
+} else {
+    echo "無效的請求";
+}
+?>
