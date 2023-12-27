6c7104fa1020c18c0e372ea278ea2d844bd66498
sharon1602 <116973213+sharon1602@users.noreply.github.com>
2023-12-28 01:17:36 +0800

    Update user_new_reservation.php

--- user_new_reservation.php
+++ user_new_reservation.php
@@ -84,8 +84,139 @@ if (isset($_POST['search']) && !empty($_POST['starttime']) && !empty($_POST['end
             background-color: #ddd;
             color: black;
         }
+
+        /* 下拉菜单樣式 */
+        .dropdown {
+            float: left;
+            overflow: hidden;
+        }
+
+        .dropdown .dropbtn {
+            font-size: 16px;
+            border: none;
+            outline: none;
+            color: white;
+            padding: 14px 20px;
+            background-color: inherit;
+            font-family: inherit;
+            margin: 0;
+        }
+
+        .navbar a:hover, .dropdown:hover .dropbtn {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            float: none;
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #ddd;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+        .dropdown-content a.active {
+            background-color: #333;
+            color: white;
+        }
+
+        /* 修正表格樣式 */
+       table {
+    width: 100%;
+    border=1
+    border-collapse: collapse;
+    margin-top: 20px; /* 調整與按鈕的間距 */
+}
+
+th, td {
+    border: 0.001px solid #6E7783; /* 調整框線顏色 */
+    padding: 8px;
+    text-align: left;
+}
+
+th {
+		background-color: #9db0c9;
+		color: 	black;
+		}
+
+		.add-button {
+		 background-color: 	#354B5E;
+		color: white;
+		padding: 10px 20px;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		text-decoration: none;
+		font-size: 16px;
+		margin: 20px; /* 調整按鈕的外邊距 */
+		}
+
+
+
+		body {
+		background-color: #ced8e4 ; /* 設定整個網頁的背景顏色 */
+		margin: 0; /* 移除預設邊距 */
+		}
+		.edit-button {
+		background-color: #feeba8;
+		color: #3e3e3e; 
+		padding: 3px 6px; /* 調整按鈕的大小 */
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		text-decoration: none;
+		font-size: 18.5px;
+		margin: 0.01px; /* 調整按鈕的外邊距 */
+		}
+
+		.edit-button:hover {
+		background-color: #4E5563; /* 在:hover時改變的背景顏色 */
+		}
+
+		.delete-button {
+		background-color: #f3dae0;
+		color: #3e3e3e; 
+		padding: 3px 6px;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		font-size: 18.5px;
+	
     
-    </style>
+		}
+		.delete-button:hover {
+		background-color: #4E5563; /* 在:hover時改變的背景顏色 */
+		}
+        .search {
+		background-color: #feeba8;
+		color: #3e3e3e; 
+		padding: 3px 6px; /* 調整按鈕的大小 */
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		text-decoration: none;
+		font-size: 18.5px;
+		margin: 0.01px; /* 調整按鈕的外邊距 */
+		}
+</style>
+   
      <!-- 其他標頭內容 -->
      <script>
 function redirectToSelectSeat() {
@@ -161,25 +292,31 @@ function redirectToSelectSeat() {
 <body> 
 <div class="navbar">
     <a href="userstatus.php">會員</a>
-    <a href="seat.php">座位</a>
+    <a href="seat.php">座位一覽</a>
     <a href="user_reservation.php">預約紀錄</a>
     <a href="user_new_reservation.php">預約座位</a>
+    <a href="search_seat.php">查詢座位</a>
+    
     <!-- 登入、登出 -->
     <a href="logout.php" style="float:right;">登出</a>
     <h4 style="float:right;"><font color="white"><?php echo $accountMessage; ?></font></h4>
    
     <!-- 可以加入其他需要的連結 -->
 </div>
-<br><br>
+
     <div class="container" style="width: 700px;margin: 0px auto; top:50px; margin-bottom 200px; font-family:Microsoft JhengHei;">
     <form id="reservationForm" class="form-signin" role="form" onsubmit="redirectToSelectSeat(); return false;">
     <!-- 開始時間 -->
     <div align="center">
+    <label for="endtime">開始時間: </label>
+    <br>
     <input type="datetime-local" class="form-control" required="required" name="starttime" id="starttime" placeholder="starttime" style="width:30%;height: 40px;">
     </div>
     <br>
     <!-- 結束時間 -->
     <div align="center">
+        <label for="endtime">結束時間:</label>
+        <br>
         <input type="datetime-local" class="form-control" required="required" name="endtime" id="endtime" placeholder="endtime" style="width:30%;height: 40px;">
     </div>
     <script>
@@ -214,11 +351,17 @@ function redirectToSelectSeat() {
             });
             </script>
     <br>
-            <div align="center"><input type="text" class="form-control" require="require" name="seatfloor" placeholder="seatfloor"  style="width:30%;height: 40px;"></div><br>
-            <div align="center"><input type="text" class="form-control" require="require" name="socket"  placeholder="socket" style="width:30%;height: 40px;"></div><br>
+            <div align="center">
+            <label for="endtime">座位樓層: </label>
+            <br>    
+            <input type="text" class="form-control" require="require" name="seatfloor" placeholder="seatfloor"  style="width:30%;height: 40px;"></div><br>
+            <div align="center">
+            <label for="endtime">座位插座: </label>    
+            <br>
+            <input type="text" class="form-control" require="require" name="socket"  placeholder="socket" style="width:30%;height: 40px;"></div><br>
             <!-- 前面的代码保持不变 -->
             <div align="center">
-        <input type="submit" value="查詢可預約座位" style="width:30%;height: 40px;">
+        <input class="search"type="submit" value="查詢可預約座位" style="width:30%;height: 40px;">
     </div>
         </Form>
     </div>
