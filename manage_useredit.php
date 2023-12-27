c9f547ae3c16bc91bd2bc4ba49739a1335b19e50
yogachiu <153476414+yogachiu@users.noreply.github.com>
2023-12-27 22:07:08 +0800

    Update manage_useredit.php

--- manage_useredit.php
+++ manage_useredit.php
@@ -2,12 +2,21 @@
 session_start();
 
 // 连接数据库
-$con = new mysqli("localhost", "root", "eva65348642", "librarydb");
+$con = new mysqli("localhost", "root", "jenny104408!", "libdb");
 
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
-
+if (isset($_SESSION['account']) ) {
+    $userId = $_SESSION['account'];
+    
+    $accountMessage = isset($_SESSION['account']) ? $_SESSION['account'] . " 您好！" : 'Hello!';
+    //$Msg="您以違規".$_SESSION['suspention']."次!";
+} else {
+    // 如果未找到用户ID，可能需要再次重定向到登录页面或者显示错误信息
+    header("Location: login.php");
+    exit;
+}
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // 通过 POST 提交
     $userId = $_POST['User_Id'];
@@ -89,8 +98,109 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>圖書館座位預約系統</title>
     <!-- 樣式 -->
+	<style>
+	.navbar {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        .navbar a {
+            float: left;
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 20px;
+            text-decoration: none;
+        }
+
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        /* 下拉菜单樣式 */
+        .dropdown {
+            float: left;
+            overflow: hidden;
+        }
+
+        .dropdown .dropbtn {
+            font-size: 16px;
+            border: none;
+            outline: none;
+            color: white;
+            padding: 14px 20px;
+            background-color: inherit;
+            font-family: inherit;
+            margin: 0;
+        }
+
+        .navbar a:hover, .dropdown:hover .dropbtn {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            float: none;
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #ddd;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+        .dropdown-content a.active {
+            background-color: #333;
+            color: white;
+        }
+		body {
+		background-color: #DCDDD8; /* 設定整個網頁的背景顏色 */
+		margin: 0; /* 移除預設邊距 */
+		}
+		.inputbtn {
+		background-color: 	#354B5E;
+		color: white;
+		padding: 3px 6px; /* 調整按鈕的大小 */
+	
+		font-size: 14.5px;
+		}
+	</style>
 </head>
 <body>
+<div class="navbar">
+        <div class="dropdown">
+            <button class="dropbtn">使用者</button>
+            <div class="dropdown-content">
+            <a <?php if (!isset($_GET['type']) || (isset($_GET['type']) && $_GET['type'] !== 'manager')) echo 'class="active"'; ?> href="manage_user.php">使用者名單</a>
+            <a <?php if (isset($_GET['type']) && $_GET['type'] === 'manager') echo 'class="active"'; ?> href="manage_user.php?type=manager">管理者名單</a>
+            <!-- 新增使用者按鈕 -->
+            <?php if (!isset($_GET['type']) || (isset($_GET['type']) && $_GET['type'] !== 'manager')) : ?>
+            <?php endif; ?>
+        </div>
+    </div>
+
+        <a href="manage_seat.php">座位狀況</a>
+        <!-- 登入、登出 -->
+        <a href="logout.php" style="float:right;">登出</a>
+		<h4 style="float:right;"><font color="white"><?php echo $accountMessage; ?></font></h4>
+    </div>
+	</div>
     <!-- 表單 -->
     <h1 style="text-align: center;">Edit User Data</h1>
     <div style="text-align: center; margin-top: 20px;">
@@ -119,7 +229,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <option value="0" <?php if ($isManager == 0) echo 'selected'; ?>>否</option>
         </select><br><br>
         <!-- 提交按钮 -->
-        <input type="submit" value="儲存修改">
+        <input class="inputbtn" type="submit" value="儲存修改">
     </form>
 </div>
 </body>
