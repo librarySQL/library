40ed998999a3170e1cdfe3cea75370e2b4471a6d
sharon1602 <116973213+sharon1602@users.noreply.github.com>
2023-12-28 01:16:04 +0800

    Update userstatus.php

--- userstatus.php
+++ userstatus.php
@@ -8,121 +8,257 @@ if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
 }
 
 // 显示用户帐户信息
-if (isset($_SESSION['account'])) {
-    $useraccount = $_SESSION['account'];
+if (isset($_SESSION['account']) ) {
+    $userId = $_SESSION['account'];
+    //$suspention=$_SESSION['suspention'];
+    // 这里你可以使用 $userId 查询数据库或其他存储来获取用户信息
+    // 在这个示例中，我们仅显示 "account 您好！" 的消息
     $accountMessage = isset($_SESSION['account']) ? $_SESSION['account'] . " 您好！" : 'Hello!';
-
-    $con = new mysqli("localhost", "root", "eva65348642", "librarydb");
-
-    if ($con->connect_error) {
-        die("Connection failed: " . $con->connect_error);
-    }
-    
-    // 添加引號以包裹帳戶名稱
-    $user = $con->query("SELECT * FROM user WHERE User_Account='$useraccount'");
-
-    echo "<!DOCTYPE html>
-    <html lang='en'>
-    <head>
-        <meta charset='UTF-8'>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <title>圖書館座位預約系統</title>
-        <style>
-            /* Navbar 樣式 */
-            .navbar {
-                overflow: hidden;
-                background-color: #333;
-            }
-
-            .navbar a {
-                float: left;
-                display: block;
-                color: white;
-                text-align: center;
-                padding: 14px 20px;
-                text-decoration: none;
-            }
-
-            .navbar a:hover {
-                background-color: #ddd;
-                color: black;
-            }
-            table {
-                width: 100%;
-                border-collapse: collapse;
-            }
-            th, td {
-                border: 1px solid #ddd;
-                padding: 8px;
-                text-align: left;
-            }
-            th {
-                background-color: #f2f2f2;
-            }
-        </style>
-    </head>
-    <body>
-        <div class='navbar'>
-            <a href='userstatus.php'>會員</a>
-            <a href='seat/seat.php'>座位</a>
-            <a href='reservation/reservation.php'>預約紀錄</a>
-            <a href='reservation/newreservation.php'>新增紀錄</a>
-            <!-- 登入、登出 -->
-            <a href='logout.php' style='float:right;'>登出</a>
-            <h4 style='float:right;'><font color='white'>$accountMessage</font></h4>
-           
-            <!-- 可以加入其他需要的連結 -->
-        </div>";
-
-    echo "<table>
-        <tr>
-            <th>帳號</th>
-            <th>密碼</th>
-            <th>抵達時間</th>
-            <th>離開時間</th>
-            <th>違規次數</th>
-            <th>停權</th>
-            <th></th>
-        </tr>";
-
-        while ($row = mysqli_fetch_array($user)) {
-            echo "<tr>";
-            echo "<td>" . $row['User_Account'] . "</td>";
-            echo "<td>" . $row['User_Password'] . "</td>";
-            echo "<td>" . $row['Arrival_Time'] . "</td>";
-            echo "<td>" . $row['Departure_Time'] . "</td>";
-            echo "<td>" . $row['Suspension'] . "</td>";
-            echo "<td>" . $row['Number_of_Violation'] . "</td>";
-            echo "<td>
-                    <button onclick='redirectToEditPage()'>編輯</button>              
-                    <button onclick=\"detailsFunction({$row['User_Id']})\">詳細</button>
-                    <button onclick=\"deleteFunction({$row['User_Id']})\">刪除</button>
-                  </td>";
-            echo "</tr>";
-        }
-        
-        echo "</table>";
-        
-        // 添加引號以包裹帳戶名稱
-        echo "</body></html>";
-        
-        // 這裡嵌入 JavaScript 函數
-        echo "<script>
-                function redirectToEditPage() {
-                    // 在這裡更改為您的編輯頁面 URL
-                    var editPageUrl = 'user/useredit.php'; // 更改成您的編輯頁面 URL
-            
-                    // 使用 JavaScript 跳轉到編輯頁面
-                    window.location.href = editPageUrl;
-                }
-              </script>";
-    echo "</table>";
-    
-    echo "</body></html>";
-
-    $con->close();
+    //$Msg="您以違規".$_SESSION['suspention']."次!";
 } else {
+    // 如果未找到用户ID，可能需要再次重定向到登录页面或者显示错误信息
     header("Location: login.php");
     exit;
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>圖書館座位預約系統</title>
+    <style>
+        /* Navbar 樣式 */
+        .navbar {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        .navbar a {
+            float: left;
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 20px;
+            text-decoration: none;
+        }
+
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        /* 下拉菜单樣式 */
+        .dropdown {
+            float: left;
+            overflow: hidden;
+        }
+
+        .dropdown .dropbtn {
+            font-size: 16px;
+            border: none;
+            outline: none;
+            color: white;
+            padding: 14px 20px;
+            background-color: inherit;
+            font-family: inherit;
+            margin: 0;
+        }
+
+        .navbar a:hover, .dropdown:hover .dropbtn {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            float: none;
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #ddd;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+        .dropdown-content a.active {
+            background-color: #333;
+            color: white;
+        }
+
+        /* 修正表格樣式 */
+       table {
+    width: 100%;
+    border=1
+    border-collapse: collapse;
+    margin-top: 20px; /* 調整與按鈕的間距 */
+}
+
+th, td {
+    border: 0.001px solid #6E7783; /* 調整框線顏色 */
+    padding: 8px;
+    text-align: left;
+}
+
+th {
+		background-color: #9db0c9;
+		color: 	black;
+		}
+
+		.add-button {
+		 background-color: 	#354B5E;
+		color: white;
+		padding: 10px 20px;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		text-decoration: none;
+		font-size: 16px;
+		margin: 20px; /* 調整按鈕的外邊距 */
+		}
+
+
+
+		body {
+		background-color: #ced8e4 ; /* 設定整個網頁的背景顏色 */
+		margin: 0; /* 移除預設邊距 */
+		}
+		.edit-button {
+            background-color: #feeba8;
+            color: #3e3e3e; /* Updated text color */
+            padding: 3px 6px; /* Adjust button size */
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            text-decoration: none;
+            font-size: 18.5px;
+            margin: 0.01px; /* Adjust button margin */
+        }
+
+    
+		.edit-button:hover {
+		background-color: #4E5563; /* 在:hover時改變的背景顏色 */
+		}
+
+		.delete-button {
+		background-color: #4F9D9D;
+		color: white;
+		padding: 3px 6px;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		font-size: 18.5px;
+	
+    
+		}
+		.delete-button:hover {
+		background-color: #4E5563; /* 在:hover時改變的背景顏色 */
+		}
+</style>
+</head>
+<body>
+<div class="navbar">
+    <a href="userstatus.php">會員</a>
+    <a href="seat.php">座位一覽</a>
+    <a href="user_reservation.php">預約紀錄</a>
+    <a href="user_new_reservation.php">預約座位</a>
+    <a href="search_seat.php">查詢座位</a>
+    
+    <!-- 登入、登出 -->
+    <a href="logout.php" style="float:right;">登出</a>
+    <h4 style="float:right;"><font color="white"><?php echo $accountMessage; ?></font></h4>
+   
+    <!-- 可以加入其他需要的連結 -->
+</div>
+<?php
+
+$con = new mysqli("localhost", "root", "ccl5266ccl", "圖書館座位預約系統");
+
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+// 检查用户是否已登录，如果未登录则重定向到登录页面
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+
+// 显示用户帐户信息
+if (isset($_SESSION['account']) ) {
+    $useraccount = $_SESSION['account'];
+
+    $accountMessage = isset($_SESSION['account']) ? $_SESSION['account'] . " 您好！" : 'Hello!';
+
+    $user = $con->query("SELECT * FROM user WHERE User_Account='$useraccount'");
+echo "<table >
+    <tr>
+        <th>帳號</th>
+        <th>密碼</th>
+        <th>抵達時間</th>
+        <th>離開時間</th>
+        <th>違規次數</th>
+        <th>停權</th>
+        <th></th>
+    </tr>";
+
+while ($row = mysqli_fetch_array($user)) {
+    echo "<tr>";
+    echo "<td>" . $row['User_Account'] . "</td>";
+    echo "<td>" . $row['User_Password'] . "</td>";
+    echo "<td>" . $row['Arrival_Time'] . "</td>";
+    echo "<td>" . $row['Departure_Time'] . "</td>";
+    echo "<td>" . $row['Suspension'] . "</td>";
+    echo "<td>" . $row['Number_of_Violation'] . "</td>";
+    echo "<td>
+    <button class='edit-button' onclick='redirectToEditPage()'>編輯</button>   
+          </td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+    //$suspention=$_SESSION['suspention'];
+    // 这里你可以使用 $userId 查询数据库或其他存储来获取用户信息
+    // 在这个示例中，我们仅显示 "account 您好！" 的消息
+  
+    //$Msg="您以違規".$_SESSION['suspention']."次!";
+} else {
+    // 如果未找到用户ID，可能需要再次重定向到登录页面或者显示错误信息
+    header("Location: login.php");
+    exit;
+}
+
+
+echo "<script>
+                function redirectToEditPage() {
+                    // 在這裡更改為您的編輯頁面 URL
+                    var editPageUrl = 'user_edit.php'; // 更改成您的編輯頁面 URL
+            
+                    // 使用 JavaScript 跳轉到編輯頁面
+                    window.location.href = editPageUrl;
+                }
+                function redirectToDeletePage() {
+                    // 在這裡更改為您的編輯頁面 URL
+                    var deletPageUrl = 'userdelete.php'; // 更改成您的編輯頁面 URL
+            
+                    // 使用 JavaScript 跳轉到編輯頁面
+                    window.location.href = deletPageUrl;
+                }
+              </script>";
+              
+?>
+</body>
+</html>
