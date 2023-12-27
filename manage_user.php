62b9cc8941a767e113c2efc565bb86bf5bb123d8
eva1617 <105513556+eva1617@users.noreply.github.com>
2023-12-27 23:23:55 +0800

    Update manage_user.php

--- manage_user.php
+++ manage_user.php
@@ -1,18 +1,16 @@
 <?php
 session_start();
 
-    $con = new mysqli("localhost", "root", "jenny104408!", "libdb");
+$con = new mysqli("localhost", "root", "eva65348642", "librarydb");
 
-    if ($con->connect_error) {
-        die("Connection failed: " . $con->connect_error);
-    }
 
-    if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
-        header("Location: login.php");
-        exit;
-    }
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+$query = "SELECT MAX(User_Id) AS maxUserId FROM user";
+$result = $con->query($query);
 
-    if (isset($_SESSION['account']) ) {
+if (isset($_SESSION['account']) ) {
     $userId = $_SESSION['account'];
     
     $accountMessage = isset($_SESSION['account']) ? $_SESSION['account'] . " 您好！" : 'Hello!';
@@ -23,16 +21,12 @@ session_start();
     exit;
 }
 
-    
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>管理使用者</title>
     <style>
-        /* Navbar 樣式 */
+       /* Navbar 樣式 */
         .navbar {
             overflow: hidden;
             background-color: #333;
@@ -103,76 +97,83 @@ session_start();
             background-color: #333;
             color: white;
         }
-
-        /* 修正表格樣式 */
-       table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-top: 20px; /* 調整與按鈕的間距 */
-}
-
-th, td {
-    border: 0.001px solid #6E7783; /* 調整框線顏色 */
-    padding: 8px;
-    text-align: left;
-}
-
-th {
-		background-color: #475F77;
-		color: 	white;
-		}
-
-		.add-button {
-		 background-color: 	#354B5E;
-		color: white;
-		padding: 10px 20px;
-		border: none;
-		border-radius: 5px;
-		cursor: pointer;
-		text-decoration: none;
-		font-size: 16px;
-		margin: 20px; /* 調整按鈕的外邊距 */
-		}
-
-
-
 		body {
 		background-color: #DCDDD8; /* 設定整個網頁的背景顏色 */
 		margin: 0; /* 移除預設邊距 */
 		}
-		.edit-button {
-		background-color: 	#354B5E;
+		.btn{
+			background-color: 	#354B5E;
 		color: white;
 		padding: 3px 6px; /* 調整按鈕的大小 */
-		border: none;
-		border-radius: 5px;
-		cursor: pointer;
-		text-decoration: none;
-		font-size: 18.5px;
-		margin: 0.01px; /* 調整按鈕的外邊距 */
-		}
-
-		.edit-button:hover {
-		background-color: #4E5563; /* 在:hover時改變的背景顏色 */
-		}
-
-		.delete-button {
-		background-color: #4F9D9D;
-		color: white;
-		padding: 3px 6px;
-		border: none;
-		border-radius: 5px;
-		cursor: pointer;
-		font-size: 18.5px;
 	
-    
+		font-size: 14.5px;
 		}
-		.delete-button:hover {
-		background-color: #4E5563; /* 在:hover時改變的背景顏色 */
-		}
-</style>
+        
+    </style>
+	
+	
+	 <script>
+        function validateForm() {
+            var isManagerValue = document.getElementById("isManager").value;
+
+            // Check if isManagerValue is not 0 or 1
+            if (isManagerValue !== "0" && isManagerValue !== "1") {
+                alert("是否為管理者？請輸入 1 （true）或 0（false）");
+                return false; // Prevent form submission
+            }
+
+            // If validation passes, allow form submission
+            return true;
+        }
+    </script>
+	
 </head>
 <body>
+
+<?php
+
+if ($result && $result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $nextUserId = $row['maxUserId'] + 1;
+} else {
+    // Default to 1 if no existing users
+    $nextUserId = 1;
+}
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userId = mysqli_real_escape_string($con, $_POST["userId"]);
+    $userAccount = mysqli_real_escape_string($con, $_POST["userAccount"]);
+    $userPassword = mysqli_real_escape_string($con, $_POST["userPassword"]);
+    $numberOfViolation = isset($_POST["numberOfViolation"]) ? mysqli_real_escape_string($con, $_POST["numberOfViolation"]) : null;
+    $suspension = isset($_POST["suspension"]) ? mysqli_real_escape_string($con, $_POST["suspension"]) : null;
+    $isManager = mysqli_real_escape_string($con, $_POST["isManager"]);
+
+    // Set to NULL if empty
+    $numberOfViolation = $numberOfViolation !== '' ? $numberOfViolation : null;
+    $suspension = $suspension !== '' ? $suspension : null;
+
+    $insertSql = "INSERT INTO user (User_Id, User_Account, User_Password, Number_of_Violation, Suspension, isManager) 
+                  VALUES ('$userId', '$userAccount', '$userPassword', ";
+    $insertSql .= $numberOfViolation !== null ? "'$numberOfViolation'" : "NULL";
+    $insertSql .= ", ";
+    $insertSql .= $suspension !== null ? "'$suspension'" : "NULL";
+    $insertSql .= ", '$isManager')";
+
+    if ($con->query($insertSql) === TRUE) {
+        // Redirect to manage_user.php
+        header("Location: manage_user.php");
+        exit;
+    } else {
+        echo "错误：" . $insertSql . "<br>" . $con->error;
+    }
+}
+
+
+
+?>
+
 <div class="navbar">
         <div class="dropdown">
             <button class="dropbtn">使用者</button>
@@ -185,70 +186,49 @@ th {
         </div>
     </div>
 
-        <a href="manage_seat.php">座位狀況</a>
+        <a href="seatdetail.php">座位狀況</a>
         <!-- 登入、登出 -->
         <a href="logout.php" style="float:right;">登出</a>
 		<h4 style="float:right;"><font color="white"><?php echo $accountMessage; ?></font></h4>
     </div>
-    <button class='add-button' onclick="location.href='manage_usercreate.php'" style='float:left; margin: 20px;'>新增使用者</button>
+	</div>
+    <h1 style="text-align: center; margin-top: 20px;">Create A New User</h1>
+<div style="text-align: center; margin-top: 20px;">
+    <form method="post" action="" onsubmit="return validateForm();">
+        <label for="userId" style="text-align: left; display: inline-block; width: 100px;">UserId：</label>
+		<input type="text" id="userId" name="userId" value="<?php echo $nextUserId; ?>" readonly style="margin-bottom: 10px;"><br>
+
+        <label for="userAccount" style="text-align: left; display: inline-block; width: 100px;">帳號：</label>
+        <input type="text" id="userAccount" name="userAccount" required style="margin-bottom: 10px;"><br>
+
+        <label for="userPassword" style="text-align: left; display: inline-block; width: 100px;">密碼：</label>
+        <input type="password" id="userPassword" name="userPassword" required style="margin-bottom: 10px;"><br>
+
+        <label for="numberOfviolation" style="text-align: left; display: inline-block; width: 100px;">違規次數：</label>
+		<input type="text" id="numberOfviolation" name="numberOfviolation" style="margin-bottom: 10px;"><br>
+
+		<label for="suspension" style="text-align: left; display: inline-block; width: 100px;">停權：</label>
+		<input type="text" id="suspension" name="suspension" style="margin-bottom: 10px;"><br>
+
+		<label for="isManager" style="text-align: left; display: inline-block; width: 100px;">管理者：</label>
+        <input type="text" id="isManager" name="isManager" required style="margin-bottom: 20px;"><br>
+
+        <!-- Add other input fields as needed -->
+
+        <input class="btn" type="submit" value="儲存">
+    </form>
 </div>
+
+<div style="text-align: center; margin-top: 30px;">
+    <a href="manage_user.php" style="text-align: left;">返回</a>
+</div>
+
+
+
+
 <?php
-    if ($con->connect_error) {
-        die("Connection failed: " . $con->connect_error);
-    }
+$con->close();
+?>
 
-    $sql = "";
-
-    if (isset($_GET['type']) && $_GET['type'] === 'manager') {
-        $sql = "SELECT * FROM user WHERE isManager = 1"; // 只顯示管理者
-    } else {
-        $sql = "SELECT * FROM user WHERE isManager = 0"; // 只顯示一般使用者
-    }
-
-    $result = $con->query($sql);
-    
-
-    if ($result && $result->num_rows > 0) {
-        echo "<table border='1'>";
-        echo "<tr><th>User_Id</th><th>帳號</th><th>密碼</th><th>抵達時間</th><th>離開時間</th><th>違規次數</th><th>停權</th><th>管理者</th><th></th>";
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>{$row['User_Id']}</td>";
-            echo "<td>{$row['User_Account']}</td>";
-            echo "<td>{$row['User_Password']}</td>";
-            echo "<td>{$row['Arrival_Time']}</td>";
-            echo "<td>{$row['Departure_Time']}</td>";
-            echo "<td>{$row['Number_of_Violation']}</td>";
-            echo "<td>{$row['Suspension']}</td>";
-            echo "<td>" . ($row['isManager'] == 1 ? '是' : '否') . "</td>";
-            echo "<td>
-            <button class='edit-button' onclick=\"redirectToEditPage('" . $row['User_Id'] . "', '" . $row['User_Account'] . "', '" . $row['User_Password'] . "', '" . $row['Number_of_Violation'] . "', '" . $row['Suspension']. "', '" . $row['isManager'] . "')\">編輯</button>
-            <button class='delete-button' onclick=\"redirectToDeletePage('" . $row['User_Id'] . "')\">刪除</button>
-            </td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "No users found.";
-    }
-
-    $con->close();
-    ?>
-<script>
-    function redirectToEditPage(userId, account, password, numberOfviolation, suspension, isManager) {
-            var editPageUrl = 'manage_useredit.php' + '?userId=' + encodeURIComponent(userId) + '&account=' + encodeURIComponent(account) + '&password=' + encodeURIComponent(password) + '&numberOfviolation=' + encodeURIComponent(numberOfviolation) + '&suspension=' + encodeURIComponent(suspension) + '&isManager=' + encodeURIComponent(isManager);
-            window.location.href = editPageUrl;
-        }
-	 function redirectToDeletePage(userId) {
-            var deleteConfirmation = confirm('是否刪除此帳號？');
-            if (deleteConfirmation) {
-                var deletePageUrl = 'manage_userdelete.php' + '?userId=' + encodeURIComponent(userId);
-                window.location.href = deletePageUrl;
-            } else {
-                // User chose to cancel, do nothing
-            }
-        }
-   
-</script>
 </body>
 </html>
