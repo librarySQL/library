1f9fc5484d58767849e0f563dcb3b6c34780163d
sharon1602 <116973213+sharon1602@users.noreply.github.com>
2023-12-28 21:03:54 +0800

    Update useredit.php

--- useredit.php
+++ useredit.php
@@ -3,21 +3,26 @@ session_start();
 
 if (isset($_SESSION['account'])) {
     $useraccount = $_SESSION['account'];
-
-    $con = new mysqli("localhost", "root", "eva65348642", "librarydb");
+    $accountMessage = isset($_SESSION['account']) ? $_SESSION['account'] . " 您好！" : 'Hello!';
+    $con = new mysqli("localhost", "root", "ccl5266ccl", "圖書館座位預約系統");
 
     if ($con->connect_error) {
         die("Connection failed: " . $con->connect_error);
     }
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // 先將 POST 的資料存入變數
         $userId = $_POST['User_Id'];
         $userAccount = $_POST['User_Account'];
         $userPassword = $_POST['User_Password'];
-    
-        // 使用 UPDATE 語句更新特定使用者資料
-        $updateQuery = "UPDATE user SET User_Account='$userAccount', User_Password='$userPassword' WHERE User_Id = '$userId'";
-    
+        //$arrivalTime = $_POST['Arrival_Time'];
+        //$departureTime = $_POST['Departure_Time'];
+        //$suspension = ($_POST['Suspension'] !== '') ? $_POST['Suspension'] : null;
+        //$numberOfViolation = $_POST['Number_of_Violation'];
+
+        // 使用 UPDATE 語句更新使用者資料
+        $updateQuery = "UPDATE user SET User_Account='$userAccount', User_Password='$userPassword' WHERE User_Id='$userId'";
+
         if ($con->query($updateQuery) === TRUE) { ?>
             <script language="javascript">
                 alert('會員資料修改完成！**請重新登入**');
@@ -27,7 +32,6 @@ if (isset($_SESSION['account'])) {
             echo "Error updating record: " . $con->error;
         }
     }
-    
 
     $user = $con->query("SELECT * FROM user WHERE User_Account='$useraccount'");
 
@@ -47,51 +51,174 @@ if (isset($_SESSION['account'])) {
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <title>圖書館座位預約系統</title>
         <style>
-            /* Navbar 樣式 */
-            .navbar {
-                overflow: hidden;
-                background-color: #333;
-            }
+        /* Navbar 樣式 */
+        .navbar {
+            overflow: hidden;
+            background-color: #333;
+        }
 
-            .navbar a {
-                float: left;
-                display: block;
-                color: white;
-                text-align: center;
-                padding: 14px 20px;
-                text-decoration: none;
-            }
+        .navbar a {
+            float: left;
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 20px;
+            text-decoration: none;
+        }
 
-            .navbar a:hover {
-                background-color: #ddd;
-                color: black;
-            }
-            table {
-                width: 100%;
-                border-collapse: collapse;
-            }
-            th, td {
-                border: 1px solid #ddd;
-                padding: 8px;
-                text-align: left;
-            }
-            th {
-                background-color: #f2f2f2;
-            }
-        </style>
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        /* 下拉菜单樣式 */
+        .dropdown {
+            float: left;
+            overflow: hidden;
+        }
+
+        .dropdown .dropbtn {
+            font-size: 16px;
+            border: none;
+            outline: none;
+            color: white;
+            padding: 14px 20px;
+            background-color: inherit;
+            font-family: inherit;
+            margin: 0;
+        }
+
+        .navbar a:hover, .dropdown:hover .dropbtn {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            float: none;
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #ddd;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+        .dropdown-content a.active {
+            background-color: #333;
+            color: white;
+        }
+
+        /* 修正表格樣式 */
+       table {
+    width: 100%;
+    border=1
+    border-collapse: collapse;
+    margin-top: 20px; /* 調整與按鈕的間距 */
+}
+
+th, td {
+    border: 0.001px solid #6E7783; /* 調整框線顏色 */
+    padding: 8px;
+    text-align: left;
+}
+
+th {
+		background-color: #9db0c9;
+		color: 	black;
+		}
+
+		.add-button {
+		 background-color: 	#354B5E;
+		color: white;
+		padding: 10px 20px;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		text-decoration: none;
+		font-size: 16px;
+		margin: 20px; /* 調整按鈕的外邊距 */
+		}
+
+
+
+		body {
+		background-color: #ced8e4 ; /* 設定整個網頁的背景顏色 */
+		margin: 0; /* 移除預設邊距 */
+		}
+		.edit-button {
+		background-color: #feeba8;
+		color: #3e3e3e; 
+		padding: 3px 6px; /* 調整按鈕的大小 */
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		text-decoration: none;
+		font-size: 18.5px;
+		margin: 0.01px; /* 調整按鈕的外邊距 */
+		}
+
+		.edit-button:hover {
+		background-color: #4E5563; /* 在:hover時改變的背景顏色 */
+		}
+
+		.delete-button {
+		background-color: #f3dae0;
+		color: #3e3e3e; 
+		padding: 3px 6px;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		font-size: 18.5px;
+	
+    
+		}
+		.delete-button:hover {
+		background-color: #4E5563; /* 在:hover時改變的背景顏色 */
+		}
+        .search {
+		background-color: #feeba8;
+		color: #3e3e3e; 
+		padding: 3px 6px; /* 調整按鈕的大小 */
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		text-decoration: none;
+		font-size: 18.5px;
+		margin: 0.01px; /* 調整按鈕的外邊距 */
+		}
+</style>
     </head>
 <body>
-    <div class='navbar'>
-        <a href='../userstatus.php'>會員</a>
-        <a href='seat/seat.php'>座位</a>
-        <a href='../reservation/reservation.php'>預約紀錄</a>
-        <a href='../reservation/reservation.php'>新增預約</a>
-        <!-- 登入、登出 -->
-        <a href='logout.php' style='float:right;'>登出</a>
-        <h4 style='float:right;'><font color='white'><?php echo isset($_SESSION['account']) ? $_SESSION['account'] . " 您好！" : 'Hello!'; ?></font></h4>
-    </div>
-
-    <!-- 表單 -->
+<div class="navbar">
+    <a href="userstatus.php">會員</a>
+    <a href="seat.php">座位一覽</a>
+    <a href="user_reservation.php">預約紀錄</a>
+    <a href="user_new_reservation.php">預約座位</a>
+    <a href="search_seat.php">查詢座位</a>
+    
+    <!-- 登入、登出 -->
+    <a href="logout.php" style="float:right;">登出</a>
+    <h4 style="float:right;"><font color="white"><?php echo $accountMessage; ?></font></h4>
+   
+    <!-- 可以加入其他需要的連結 -->
+</div>
+<div >
+<div align="center">
     <h1>Edit User Data</h1>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <!-- 使用者 ID 作為隱藏輸入 -->
@@ -102,20 +229,9 @@ if (isset($_SESSION['account'])) {
         
         <label for="User_Password">密碼：</label>
         <input type="password" id="User_Password" name="User_Password" value="<?php echo $row['User_Password']; ?>"><br><br>
-        <!--
-        <label for="Arrival_Time">抵達時間：</label>
-        <input type="datetime-local" id="Arrival_Time" name="Arrival_Time" value="<?php echo date('Y-m-d\TH:i', strtotime($row['Arrival_Time'])); ?>"><br><br>
-
-        <label for="Departure_Time">離開時間：</label>
-        <input type="datetime-local" id="Departure_Time" name="Departure_Time" value="<?php echo date('Y-m-d\TH:i', strtotime($row['Departure_Time'])); ?>"><br><br>
-
-        <label for="Suspension">停權：</label>
-        <input type="number" id="Suspension" name="Suspension" value="<?php echo $row['Suspension']; ?>"><br><br>
-
-        <label for="Number_of_Violation">違規次數：</label>
-        <input type="number" id="Number_of_Violation" name="Number_of_Violation" value="<?php echo $row['Number_of_Violation']; ?>"><br><br>
-        -->  
-        <input type="submit" value="儲存修改">
+       
+        <input class="edit-button " type="submit" value="儲存修改">
     </form>
+</div>
 </body>
 </html>
